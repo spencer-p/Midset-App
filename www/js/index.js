98366d6cfd32136eb0b2639cedcb3966aa420656
Spencer Peterson <spencerjpeterson@gmail.com>
2017-06-18 23:47:02 -0700

    start to build ui

--- www/js/index.js
+++ www/js/index.js
@@ -16,22 +16,26 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+var app = function() {
+
+	var self = {};
+
     // Application Constructor
-    initialize: function() {
+    self.initialize = function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+		$("#app").show();
+    };
 
     // deviceready Event Handler
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+    self.onDeviceReady = function() {
+        self.receivedEvent('deviceready');
+    };
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    self.receivedEvent = function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -40,7 +44,50 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    };
+
+	self.onClickAdd = function() {
+		console.log("onClickAdd()");
+		// Insert another empty set at the end
+		self.vue.sets.splice(self.vue.sets.length, 1, {
+			id: self.vue.sets.length,
+			text: "",
+			show_more: false
+		});
+	};
+
+	self.onClickSet = function(id) {
+		console.log("onClickSet()");
+		self.vue.sets[id].show_more = !self.vue.sets[id].show_more;
+	};
+
+	self.onClickEditSet = function(id) {
+		console.log("onClickEditSet()");
+	};
+	
+	self.vue = new Vue({
+		el: "#app",
+		data: {
+			sets: [
+				{id: 0, text: "Left right up down", show_more: false},
+				{id: 1, text: "Front of the front hash or other stuff", show_more: false}	
+			],
+			title: "Midset Calculator"
+		},
+		methods: {
+			onClickAdd: self.onClickAdd,
+			onClickSet: self.onClickSet,
+			onClickEditSet: self.onClickEditSet
+		}
+	});
+
+	return self;
 };
 
-app.initialize();
+var APP = null;
+
+jQuery(function() {
+	APP = app();
+	APP.initialize();
+});
+
