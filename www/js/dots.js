07cb4331bde3c74b8a2229eab8a811146f7850b9
Spencer Peterson <spencerjpeterson@gmail.com>
2017-06-25 12:52:58 -0700

    add hash property for dots

--- www/js/dots.js
+++ www/js/dots.js
@@ -229,6 +229,19 @@ class Dot {
 		return `${this.sideText} ${this.leftToRightText} ${this.frontToBackText}`;
 	}
 
+	get hash() {
+		var string = this.humanReadable;
+		var hash = 0;
+		if (string.length == 0) return hash;
+		for (var i = 0; i < string.length; i++) {
+			var char = string.charCodeAt(i);
+			hash = ((hash<<5)-hash)+char;
+			hash = hash & hash; // Convert to 32bit integer
+
+		}
+		return hash;
+	}
+
 }
 
 //a = new Dot({
